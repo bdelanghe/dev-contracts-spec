3ae8dcbf133233d03922b2c1bcb987e1ad451abd
Robert DeLanghe <1240090+bdelanghe@users.noreply.github.com>
2025-04-12 13:58:09 -0400

    simple start

--- main.ts
+++
@@ -1,9 +0,0 @@
-/**
- * This is the main entry point for the dev-contracts-spec module.
- */
-
-console.log("Hello from dev-contracts-spec!");
-
-export function greet(name: string = "world"): string {
-  return `Hello, ${name}!`;
-} 
